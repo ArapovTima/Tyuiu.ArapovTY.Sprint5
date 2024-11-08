f0ff321670a1bae882543fb37fd9e7fb48f53e29
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-11-08 20:56:05 +0500

    Task0

--- Tyuiu.ArapovTY.Sprint5.Task0.V7.Test/DataServiceTest.cs
+++ Tyuiu.ArapovTY.Sprint5.Task0.V7.Test/DataServiceTest.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.ArapovTY.Sprint5.Task0.V7.Test
         [TestMethod]
         public void ValidSaveToFileTextData()
         {
-            string path = @"C:\Users\user\source\repos\Tyuiu.ArapovTY.Sprint5\Tyuiu.ArapovTY.Sprint5.Task0.V7\bin\Debug\net8.0\OutPutFileTask0.txt";
+            string path = @"C:\Users\user\source\repos\Tyuiu.ArapovTY.Sprint5\Tyuiu.ArapovTY.Sprint5.Task0.V7\bin\Debug\net8.0\OutPutFileTask0.txt.txt";
             FileInfo fileInfo = new FileInfo(path);
             bool fileExists = fileInfo.Exists;
             bool wait = true;

5bf2153db387afc8919c20372651a3638c697b9b
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-11-08 21:11:58 +0500

    Task0

--- Tyuiu.ArapovTY.Sprint5.Task0.V7.Lib/DataService.cs
+++ Tyuiu.ArapovTY.Sprint5.Task0.V7.Lib/DataService.cs
@@ -7,7 +7,7 @@ namespace Tyuiu.ArapovTY.Sprint5.Task0.V7.Lib
         public string SaveToFileTextData(int x)
         {
             string path = Path.Combine(new string[] { Path.GetTempPath(), "OutPutFileTask0.txt" });
-            double z = (-(Math.Pow(x, 3))) + 4 * (Math.Pow(x, 2)) - ((3 / 2) * x);
+            double z = -(Math.Pow(x, 3)) + 4 * (Math.Pow(x, 2)) - (3 / 2 * x);
             z = Math.Round(z, 3);
             File.WriteAllText(path, Convert.ToString(z));
             return path;

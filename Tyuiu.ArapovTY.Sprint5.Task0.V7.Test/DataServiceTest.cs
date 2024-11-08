c3ccc3f5b152fcf3417d897c7827ec6af1692a4c
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-11-08 20:53:52 +0500

    Add project files.

---
+++ Tyuiu.ArapovTY.Sprint5.Task0.V7.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using Tyuiu.ArapovTY.Sprint5.Task0.V7.Lib;
+using System.IO;
+namespace Tyuiu.ArapovTY.Sprint5.Task0.V7.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidSaveToFileTextData()
+        {
+            string path = @"C:\Users\user\source\repos\Tyuiu.ArapovTY.Sprint5\Tyuiu.ArapovTY.Sprint5.Task0.V7\bin\Debug\net8.0\OutPutFileTask0.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}

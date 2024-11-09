a1999b1a0ee9326052f68cccef37304ee34b6d28
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-11-09 17:34:05 +0500

    Task2

--- Tyuiu.ArapovTY.Sprint5.Task2.V18.Lib/DataService.cs
+++ Tyuiu.ArapovTY.Sprint5.Task2.V18.Lib/DataService.cs
@@ -19,7 +19,7 @@ namespace Tyuiu.ArapovTY.Sprint5.Task2.V18.Lib
             {
                 for (int j = 0; j < columns; j++)
                 {
-                    if (matrix[i, j] < 0)
+                    if (matrix[i, j] <= 0)
                     {
                         matrix[i, j] = 0;
                     }

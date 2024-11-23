372fa8cc0725299311c810ec17fe3e68b671bf0f
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-11-23 21:54:58 +0500

    Task5

---
+++ Tyuiu.ArapovTY.Sprint5.Task5.V9.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using tyuiu.cources.programming.interfaces.Sprint5;
+namespace Tyuiu.ArapovTY.Sprint5.Task5.V9.Lib
+{
+    public class DataService : ISprint5Task5V9
+    {
+        public double LoadFromDataFile(string path)
+        {
+            {
+                string strX = File.ReadAllText(path);
+                FileInfo fileInfo = new FileInfo(path);
+                strX = strX.Replace(".", ",");
+                double max = strX.Max();
+                return max;
+            }
+        }
+    }
+}
---
+++ Tyuiu.ArapovTY.Sprint5.Task5.V9.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+using System.IO;
+using Tyuiu.ArapovTY.Sprint5.Task5.V9.Lib;
+namespace Tyuiu.ArapovTY.Sprint5.Task5.V9.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask5V9.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}

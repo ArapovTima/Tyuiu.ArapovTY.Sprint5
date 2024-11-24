39f743a0254939369d60783fafd42a9950530586
Тимофей Арапов <Tyuiu.ArapovTY@outlook.com>
2024-11-24 18:07:30 +0500

    Task6

---
+++ Tyuiu.ArapovTY.Sprint5.Task6.V11.Lib/DataService.cs
@@ -0,0 +1,34 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint5;
+using System.IO;
+namespace Tyuiu.ArapovTY.Sprint5.Task6.V11.Lib
+{
+    public class DataService : ISprint5Task6V11
+    {
+        public int LoadFromDataFile(string path)
+        {
+            int count = 0;
+            // Открываем файл с использованием StreamReader
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string line;
+                // Читаем файл построчно
+                while ((line = reader.ReadLine()) != null)
+                {
+                    // Разделяем строку на слова по пробелам и другим разделителям
+                    string[] words = line.Split(new char[] { ' ', ',', '.', '!', '?', ';', ':', '-', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    // Подсчитываем количество слов длиной 6 символов
+                    foreach (var word in words)
+                    {
+                        if (word.Length == 6)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count; // Возвращаем результат
+        }
+        
+    }
+}
---
+++ Tyuiu.ArapovTY.Sprint5.Task6.V11.Test/DataServiceTest.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using Tyuiu.ArapovTY.Sprint5.Task6.V11.Lib;
+namespace Tyuiu.ArapovTY.Sprint5.Task6.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalc()
+        {
+            DataService ds = new DataService();
+            string path = @"C:\DataSprint5\InPutDataFileTask6V11.txt";
+            var res = ds.LoadFromDataFile(path);
+            int wait = 3;
+            Assert.AreEqual(wait, res);
+        }
+        [TestMethod]
+        public void ValidLoadFromDataFile()
+        {
+            string path = @"C:\DataSprint5\InPutDataFileTask6V11.txt";
+            FileInfo fileInfo = new FileInfo(path);
+            bool fileExists = fileInfo.Exists;
+            bool wait = true;
+            Assert.AreEqual(wait, fileExists);
+        }
+    }
+}
